9c537d4c5fb27d6b516ff2bbbddd061b194704d5
kbsmith01 <kbsmith01@TS15849.ysu.local>
2018-01-19 12:37:23 -0500

    String characters out.

--- src/java_class_prg_00_03_packages/JAVA_CLASS_PRG_00_03_Packages.java
+++ src/java_class_prg_00_03_packages/JAVA_CLASS_PRG_00_03_Packages.java
@@ -15,7 +15,13 @@ public class JAVA_CLASS_PRG_00_03_Packages {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String name = new String("Fred");
+        //System.out.println(name.charAt(2));
+        
+        for(int i = 0 ; i < name.length() ; i++) {
+            System.out.println(name.charAt(i));
+        }
     }
     
 }
